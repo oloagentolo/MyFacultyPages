cec8a18b0f0ea6b3b93564d1d912e8cf65e3b101
Ryan <oloagentolo@gmail.com>
2013-07-31 11:46:36 -0500

    Added foreign key column to courses successfully

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130731022421) do
+ActiveRecord::Schema.define(version: 20130731164512) do
 
   create_table "courses", force: true do |t|
     t.string   "dept_code"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20130731022421) do
     t.text     "summary"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "faculty_member_id"
   end
 
   create_table "faculty_members", force: true do |t|

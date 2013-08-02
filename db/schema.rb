c26e92ca3966f0235debc6f4a1ee93ae8dfe1998
Ryan <oloagentolo@gmail.com>
2013-08-01 20:54:04 -0500

    Added image url migration to publications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130802010805) do
+ActiveRecord::Schema.define(version: 20130802014640) do
 
   create_table "courses", force: true do |t|
     t.string   "dept_code"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20130802010805) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "year"
+    t.string   "image_url"
   end
 
   create_table "researches", force: true do |t|
